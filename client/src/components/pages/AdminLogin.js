267e8f523d54550eefb98bdd7a6ed728599cd7b3
yashakabra <yashakabra@gmail.com>
2023-03-25 04:08:26 +0530

    Made admin login

--- client/src/components/pages/AdminLogin.js
+++ client/src/components/pages/AdminLogin.js
@@ -1,54 +1,68 @@
 import React from 'react';
+import { Alert } from 'react-bootstrap';
+import { useNavigate } from 'react-router';
 import '../Styles/UserLogin.css';
+import { useState } from 'react';
 
 const Adminlogin = () => {
-  return (
-    <>
-        
-        <div className="maincontainer">
-        <div class="container-fluid">
-            <div class="row no-gutter">
-               
-                <div class="col-md-6 d-none d-md-flex bg-image">
-                <img alt="" style={{ maxHeight: 650, maxWidth: 700 ,marginLeft:"10%"}} />
-                </div>
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
 
 
-                
-                <div class="col-md-6 bg-light ">
-                    <div class="login d-flex align-items-center py-5">
+    const submitHandler = async (event) => {
+        event.preventDefault();
+        console.log(email, password);
+        console.log(process.env.REACT_APP_ADMIN_EMAIL_ID, process.env.REACT_APP_ADMIN_PASSWORD);
+        if((email === process.env.REACT_APP_ADMIN_EMAIL_ID )&& (password === process.env.REACT_APP_ADMIN_PASSWORD)){
+            navigate('/adminpanel');
+        }else{
+            setError("INVALID CREDENTIALS");
+        }
+    }
 
-                       
-                        <div class="container mt-5">
-                          
-                            <div class="row">
-                                <div class="col-lg-10 col-xl-7 mx-auto mt-5">
-                                    <h3 class="text-center display-4" style={{color:"#0095b6"}}>Admin Login</h3>
-                                    
-                                    <form>
-                                        <div class="form-group mb-3 mt-3">
-                                            <input id="inputEmail" type="email" placeholder="Email address" required="" autofocus="" class="form-control rounded-pill border-0 shadow-sm px-4" />
+
+    return (
+        <>
+            <div className="maincontainer">
+                <div class="container-fluid">
+                    <div class="row no-gutter">
+                        <div class="col-md-6 d-none d-md-flex bg-image">
+                            <img alt="" style={{ maxHeight: 650, maxWidth: 700, marginLeft: "10%" }} />
+                        </div>
+                        <div class="col-md-6 bg-light ">
+                            <div class="login d-flex align-items-center py-5">
+                                <div class="container mt-5">
+
+                                    <div class="row">
+                                        <div class="col-lg-10 col-xl-7 mx-auto mt-5">
+                                            <h3 class="text-center display-4" style={{ color: "#0095b6" }}>Admin Login</h3>
+                                            {error && <Alert variant='danger'>{error}</Alert>}
+                                            <form onSubmit={submitHandler}>
+                                                <div class="form-group mb-3 mt-3">
+                                                    <input onChange={(e) => { setEmail(e.target.value) }} id="inputEmail" type="email" placeholder="Email address" required="" autofocus="" class="form-control rounded-pill border-0 shadow-sm px-4" />
+                                                </div>
+                                                <div class="form-group mb-3">
+                                                    <input onChange={(e) => { setPassword(e.target.value) }} id="inputPassword" type="password" placeholder="Password" required="" class="form-control rounded-pill border-0 shadow-sm px-4 text-primary" />
+                                                </div>
+
+                                                <div class="text-center">
+                                                    <button type="submit" class="btn  btn-block text-uppercase mb-2 rounded-pill shadow-sm" style={{ background: "#0095b6", color: "white" }}>Sign in</button>
+                                                </div>
+                                            </form>
                                         </div>
-                                        <div class="form-group mb-3">
-                                            <input id="inputPassword" type="password" placeholder="Password" required="" class="form-control rounded-pill border-0 shadow-sm px-4 text-primary" />
-                                        </div>
-                                       
-                                        <div class="text-center">
-                                        <button type="submit" class="btn  btn-block text-uppercase mb-2 rounded-pill shadow-sm" style={{background:"#0095b6",color:"white"}}>Sign in</button>
-                                        </div>
-                                    </form>
+                                    </div>
                                 </div>
+
                             </div>
                         </div>
-
                     </div>
                 </div>
-
             </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 
 }
 
